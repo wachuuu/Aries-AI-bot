dc0b4f84d47deca2f4463f2f29fb82bc1ee1fa45
wachuuu <gab.wachowski15@gmail.com>
2022-01-17 12:54:22 +0100

    new negamax

--- Players/NullPlayer/src/main/java/put/ai/games/naiveplayer/NicePlayer.java
+++ Players/NullPlayer/src/main/java/put/ai/games/naiveplayer/NicePlayer.java
@@ -26,9 +26,6 @@ class MyMove {
 
 public class NicePlayer extends Player {
 
-    private Color myColor;
-    private Color enemyColor;
-    private int boardSize;
     private long timeStart;
     private long timeStop;
     static Random random = new Random(0xCAFFE);
@@ -50,6 +47,7 @@ public class NicePlayer extends Player {
     }
 
     Integer getMoveValue(Board b, Color color) {
+        int boardSize = b.getSize();
         int value = 0;
         if (b.getWinner(color) == color) return Integer.MAX_VALUE;
 
@@ -61,13 +59,13 @@ public class NicePlayer extends Player {
                        value += i * j;
                    }
                    if (color == Color.PLAYER2) {
-                       value += (this.boardSize + 1 - i) * (this.boardSize + 1 - j);
+                       value += (boardSize + 1 - i) * (boardSize + 1 - j);
                    }
                }
                if (b.getState(i-1, j-1) == getOpponent(color)) {
                    value -= 1000;
                    if (color == Color.PLAYER1) {
-                       value -= (this.boardSize + 1 - i) * (this.boardSize + 1 - j);
+                       value -= (boardSize + 1 - i) * (boardSize + 1 - j);
                    }
                    if (color == Color.PLAYER2) {
                        value -= i * j;
@@ -82,47 +80,37 @@ public class NicePlayer extends Player {
         Color nextColor = getOpponent(color);
         List<Move> nextMoves = b.getMovesFor(color);
         Collections.shuffle(nextMoves);
-        if (depth == 0 || nextMoves.isEmpty() || hasTimeEnded())
-            return new MyMove(null, getMoveValue(b, color));
 
-        MyMove bestMove = null;
+        if (depth == 0 || nextMoves.isEmpty() || hasTimeEnded()) return new MyMove(null, getMoveValue(b, color));
+        if (b.getWinner(color) == color) return new MyMove(null, Integer.MAX_VALUE);
+        if (b.getWinner(color) == getOpponent(color)) return new MyMove(null, Integer.MIN_VALUE);
+
+        MyMove bestMove = new MyMove(null, Integer.MIN_VALUE);
         for (Move move : nextMoves) {
             b.doMove(move);
-            MyMove nextMove = negamax(b, nextColor, depth - 1, -beta, -alpha);
-            b.undoMove(move);
-            if (nextMove.value == null) continue;
+            MyMove nextMove = negamax(b, nextColor,depth-1, -beta, -alpha);
             nextMove.value *= -1;
             nextMove.move = move;
-            if (bestMove == null || nextMove.value > bestMove.value) bestMove = nextMove;
-            alpha = Math.max(bestMove.value, alpha);
+            b.undoMove(move);
+
+            if (nextMove.value > bestMove.value) bestMove = nextMove;
+            if (bestMove.value > alpha) alpha = bestMove.value;
             if (alpha >= beta) {
-                bestMove.value = beta;
-                return  bestMove;
-            };
+                bestMove.value = alpha;
+                return bestMove;
+            }
         }
-        bestMove.value = alpha;
         return bestMove;
     }
 
+
     MyMove getBestMove(Board b, Color color) {
         MyMove bestMove = getRandomMove(b, color);
-        for (int depth = 1; depth <= this.depthLimit && !hasTimeEnded(); depth++) {
-            MyMove candidateMove = negamax(b, color, depth, Integer.MIN_VALUE, Integer.MAX_VALUE);
-            if (candidateMove.value > bestMove.value) bestMove = candidateMove;
-        }
-
+        bestMove = negamax(b, color, this.depthLimit, Integer.MIN_VALUE, Integer.MAX_VALUE);
         System.out.println("("+bestMove.value+")\t"+bestMove.move);
         return bestMove;
     }
 
-    private void assignColors() {
-        this.myColor = getColor();
-        this.enemyColor = getOpponent(getColor());
-    }
-
-    private void assignBoardSize(Board board) {
-        this.boardSize = board.getSize();
-    }
 
     private void assignTime() {
         this.timeStart = System.currentTimeMillis();
@@ -131,12 +119,8 @@ public class NicePlayer extends Player {
 
     @Override
     public Move nextMove(Board b) {
-        this.assignBoardSize(b);
-        this.assignColors();
-        this.assignTime();
-
-        MyMove nextMove = getBestMove(b, this.myColor);
-        b.doMove(nextMove.move);
+        assignTime();
+        MyMove nextMove = getBestMove(b, getColor());
         return nextMove.move;
     }
 }
